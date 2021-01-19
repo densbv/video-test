155aebaf907c2e6bed3f3a69362ff96125d4d5e9
densbv <63791038+densbv@users.noreply.github.com>
2021-01-19 17:41:05 +0700

    Update app001.js

--- public_html/app001.js
+++ public_html/app001.js
@@ -10,10 +10,10 @@ const apiUrl = "https://orionnet.online/api/v2/cameras/public";
 const camsOnePage = 9;
 
 const indexes = (page, camsOnePage) => {
-    let end = ((page * camsOnePage) - 1);
-    let start = page > 1 ? end - camsOnePage : 0;
-    return [start, end];
-}
+  let end = page * camsOnePage - 1;
+  let start = page > 1 ? end - camsOnePage : 0;
+  return [start, end];
+};
 const [...items] = indexes(page, camsOnePage);
 console.log(items);
 
@@ -21,20 +21,14 @@ fetch(apiUrl)
   .then((res) => res.json())
   .then(
     (result) => {
-
       let pages = result.length / camsOnePage;
       console.log(Math.floor(pages));
-      
+
       cams = result.slice(...items);
       console.log(cams);
-
-      for (let i=0; cams.length; i++) {
-        console.log(cams[i].title)
-      }
 
     },
     (error) => {
       console.log(error);
     }
   );
-
