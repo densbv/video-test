2f88131ea08acfa8f30ba521c4db743ac4fba97e
densbv <63791038+densbv@users.noreply.github.com>
2021-01-19 16:07:18 +0700

    Update app001.js

--- public_html/app001.js
+++ public_html/app001.js
@@ -1,3 +1,26 @@
-console.log("hello app");
+
+let params = new URLSearchParams(document.location.search.substring(1));
+let page = parseInt(params.get("page"));
+if (isNaN(page)) {
+    page = 1;
+}
+console.log(page);
 
 let apiUrl = "https://orionnet.online/api/v2/cameras/public";
+
+const count = 9;
+
+fetch(apiUrl)
+  .then((res) => res.json())
+  .then(
+    (result) => {
+      console.log(result[0]);
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+
+  function renderCam () {
+
+  }
