18a7145a78b456ffade2955d169ae59a4f253404
densbv <63791038+densbv@users.noreply.github.com>
2021-01-19 17:37:52 +0700

    Update app001.js

--- public_html/app001.js
+++ public_html/app001.js
@@ -1,26 +1,40 @@
-
-let params = new URLSearchParams(document.location.search.substring(1));
+const params = new URLSearchParams(document.location.search.substring(1));
 let page = parseInt(params.get("page"));
 if (isNaN(page)) {
-    page = 1;
+  page = 1;
 }
 console.log(page);
 
-let apiUrl = "https://orionnet.online/api/v2/cameras/public";
+const apiUrl = "https://orionnet.online/api/v2/cameras/public";
 
-const count = 9;
+const camsOnePage = 9;
+
+const indexes = (page, camsOnePage) => {
+    let end = ((page * camsOnePage) - 1);
+    let start = page > 1 ? end - camsOnePage : 0;
+    return [start, end];
+}
+const [...items] = indexes(page, camsOnePage);
+console.log(items);
 
 fetch(apiUrl)
   .then((res) => res.json())
   .then(
     (result) => {
-      console.log(result[0]);
+
+      let pages = result.length / camsOnePage;
+      console.log(Math.floor(pages));
+      
+      cams = result.slice(...items);
+      console.log(cams);
+
+      for (let i=0; cams.length; i++) {
+        console.log(cams[i].title)
+      }
+
     },
     (error) => {
       console.log(error);
     }
   );
 
-  function renderCam () {
-
-  }
